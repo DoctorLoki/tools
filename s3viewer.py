2f102804b27403c624942f2859ede99856db0a48
Lachlan Patrick <lachlan.patrick@nearmap.com>
2021-07-29 16:05:30 +1000

    Update s3viewer.py to copy files, and use Python3.

--- s3viewer.py
+++ s3viewer.py
@@ -12,13 +12,15 @@
 
 import os, sys, string
 
-CmdFmt       = "aws s3 ls {PATH} 2>/dev/null"
-CmdFmtPretty = "aws s3 ls {PATH}"
+ListCmdFmt       = "aws s3 ls {PATH} 2>/dev/null"
+ListCmdFmtPretty = "aws s3 ls {PATH}"
+CopyCmdFmt       = "aws s3 cp {PATH} {DESTPATH}"
 ItemFmt = "% 5d) %-10s %-8s % 12s %s"
 
 def main():
 	cache = {}
 	levels = []
+	print(cmdfmt("", "", ListCmdFmtPretty))
 	folders, files = ls("")
 	cache[""] = (folders, files)
 	while True:
@@ -29,33 +31,53 @@ def main():
 		for date, time, size, name in files:
 			i += 1
 			print(ItemFmt % (i, date, time, size, name))
-		print("% 5d) back/exit" % 0)
+		if len(levels) == 0:
+			print("% 5d) exit" % 0)
+		else:
+			print("% 5d) back" % 0)
 		while True:
 			try:
 				sys.stdout.write("Choose: ")
+				sys.stdout.flush()
 				result = sys.stdin.readline()
 				num = int(result)
 				if num == 0:
+					# 0 means go up a level, i.e. cd ..
 					if len(levels) == 0:
 						return
 					levels = levels[:-1]            # cd ..
 					break
 				num -= 1 # ignore 0 option
 				if num < len(folders):
+					# Folders are listed first, so choosing one means go there, i.e. cd foldername
 					levels.append(folders[num][-1]) # cd foldername
 					break
 				num -= len(folders)
 				if num < len(files):
-					print("Filename: %s" % (files[num][-1]))
-					continue
+					# Files are listed next, choosing one asks whether to copy it to local disk.
+					sys.stdout.write("Copy file: %s ? (y/n) " % (files[num][-1]))
+					sys.stdout.flush()
+					if sys.stdin.readline().strip().lower()[:1] != 'y':
+						continue
+					# Copy the file to the local file system, within a created directory tree.
+					path = "/".join(levels)
+					destpath = make_all_dirs(path)
+					if destpath == None:
+						continue
+					cmd = cmdfmt(path, destpath, CopyCmdFmt)
+					print(cmd)
+					os.system(cmd)
+					break
 				print("Invalid option")
 			except:
 				print("\nExiting...")
 				return
-		path = string.join(levels, "/")
+
+		# List folders and files.
+		path = "/".join(levels)
 		if path and path[-1] != '/':
 			path += "/"
-		print(cmdfmt(path, CmdFmtPretty))
+		print(cmdfmt(path, "", ListCmdFmtPretty))
 		if path in cache:
 			folders, files = cache[path]
 		else:
@@ -63,7 +85,7 @@ def main():
 			cache[path] = (folders, files)
 
 def ls(path):
-	cmd = cmdfmt(path)
+	cmd = cmdfmt(path, "", ListCmdFmt)
 	try:
 		f = os.popen(cmd)
 		lines = f.readlines()
@@ -101,14 +123,46 @@ def ls(path):
 		continue
 	return folders, files
 
-def cmdfmt(path="", fmt=CmdFmt):
-	if path != "" and path[:5] != "s3://":
+def cmdfmt(path="", destpath="", fmt=ListCmdFmt):
+	if path[:5] != "s3://":
 		path = "s3://" + path
 	if path:
 		cmd = fmt.replace("{PATH}", "'" + path + "'")
 	else:
 		cmd = fmt.replace("{PATH}", "")
+	if destpath:
+		cmd = cmd.replace("{DESTPATH}", "'" + destpath + "'")
+	else:
+		cmd = cmd.replace("{DESTPATH}", ".")
 	return cmd
+
+def make_all_dirs(path=""):
+	if path != "" and path[:5] == "s3://":
+		path = path[5:]
+	destpath = "./"
+	for subdir in path.split('/'):
+		destpath += subdir + "/"
+		try:
+			os.listdir(destpath)
+			continue
+		except:
+			print("mkdir '%s'" % (destpath))
+		if not mkdir(destpath):
+			print("Could not create destination path " + destpath)
+			return None
+	return destpath
+
+def mkdir(dirpath):
+	try:
+		os.listdir(dirpath)
+		return True # Already exists, good.
+	except:
+		pass
+	try:
+		os.mkdir(dirpath)
+		return True # Created, good.
+	except:
+		return False # Failed to create, bad.
 
 if __name__ == "__main__":
 	main()
