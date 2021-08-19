20aa0d2d7f814950b8088160773566dfab7c7bb9
Lachlan Patrick <lachlan.patrick@nearmap.com>
2021-08-19 17:35:33 +1000

    Add ownership display for files to S3 viewer.

--- s3viewer.py
+++ s3viewer.py
@@ -11,11 +11,12 @@
 # Then run this program to list the S3 contents available to you.
 
 import os, sys, string
+import json
 
 ListCmdFmt       = "aws s3 ls {PATH} 2>/dev/null"
 ListCmdFmtPretty = "aws s3 ls {PATH}"
 CopyCmdFmt       = "aws s3 cp {PATH} {DESTPATH}"
-ItemFmt = "% 5d) %-10s %-8s % 12s %s"
+ItemFmt = "% 5d) %-15s % 12s %-10s %-8s %s"
 
 def main():
 	cache = {}
@@ -23,19 +24,20 @@ def main():
 	print(cmdfmt("", "", ListCmdFmtPretty))
 	folders, files = ls("")
 	cache[""] = (folders, files)
-	filtertext = None
 	while True:
+		filtertext = None
+		include_owner = False
 		i = 0
-		for date, time, size, name in folders:
+		for date, time, owner, size, name in folders:
 			i += 1
 			if filtertext and filtertext not in name:
 				continue
-			print(ItemFmt % (i, date, time, "", name + "/"))
-		for date, time, size, name in files:
+			print(ItemFmt % (i, owner, "", date, time, name + "/"))
+		for date, time, owner, size, name in files:
 			i += 1
 			if filtertext and filtertext not in name:
 				continue
-			print(ItemFmt % (i, date, time, size, name))
+			print(ItemFmt % (i, owner, size, date, time, name))
 		if len(levels) == 0:
 			print("% 5d) exit" % 0)
 		else:
@@ -45,10 +47,21 @@ def main():
 			try:
 				sys.stdout.write("Choose: ")
 				sys.stdout.flush()
-				result = sys.stdin.readline()
-				if result and result[:1] == "/" and result[-1:] == "\n":
+				result = ""
+				while result == "": 
+					result = sys.stdin.readline()
+					result = result.strip()
+				if result and result[:1] == "/":
 					# /text means search for that text in the list of folders and files.
-					filtertext = result[1:-1]
+					filtertext = result[1:]
+					break
+				if result == "ls -la":
+					# ls -la means include ownership data in listings from now on.
+					if path in cache:
+						folder, files = cache[path]
+						if not files or not files[0] or not files[0][2]: # owner is missing
+							del cache[path] # remove cached entry to force loading of owner info
+					include_owner = True
 					break
 				num = int(result)
 				if num == 0:
@@ -92,10 +105,43 @@ def main():
 		if path in cache:
 			folders, files = cache[path]
 		else:
-			folders, files = ls(path)
+			folders, files = ls(path, include_owner)
 			cache[path] = (folders, files)
 
-def ls(path):
+def get_owner(bucket, key):
+	cmd = "aws s3api get-object-acl --bucket %s --key %s" % (bucket, key)
+	try:
+		print(cmd)
+		f = os.popen(cmd)
+		data = f.read()
+		f.close()
+	except:
+		print("Could not popen %s" % (cmd))
+		return None
+
+	try:
+		j = json.loads(data)
+		if "Owner" in j and "DisplayName" in j["Owner"]:
+			return j["Owner"]["DisplayName"]
+	except:
+		pass
+	return None
+
+def get_bucket(path):
+	if path and path[:5] == "s3://":
+		path = path[5:]
+	if "/" not in path:
+		return path
+	return path[:path.find("/")]
+
+def get_key(path):
+	if path and path[:5] == "s3://":
+		path = path[5:]
+	if "/" not in path:
+		return ""
+	return path[path.find("/")+1:]
+
+def ls(path, include_owner=False):
 	cmd = cmdfmt(path, "", ListCmdFmt)
 	try:
 		f = os.popen(cmd)
@@ -112,23 +158,31 @@ def ls(path):
 			line = line[:-1]
 		parts = line.split()
 		if len(parts) == 2 and parts[0] == "PRE":
+			# Buckets
 			foldername = parts[-1].strip()
 			if foldername[-1] == '/':
 				foldername = foldername[:-1]
-			folders.append(("", "", "", foldername))
+			date = time = owner = size = ""
+			folders.append((date, time, owner, size, foldername))
 			continue
 		elif len(parts) == 3:
+			# Folders
 			date, time, size, foldername = parts[0], parts[1], None, parts[-1]
 			if foldername[-1] == '/':
 				foldername = foldername[:-1]
-			folders.append((date, time, size, foldername))
+			owner = ""
+			folders.append((date, time, owner, size, foldername))
 			continue
 		elif len(parts) >= 4:
+			# Files
 			date, time, size = parts[0], parts[1], parts[2]
 			pos = line.find(size, len(date)+1+len(time)+1)
 			if pos > 0:
 				filename = line[pos+len(size)+1:]
-				files.append((date, time, size, filename))
+				owner = ""
+				if include_owner:
+					owner = get_owner(get_bucket(path), get_key(path + filename))
+				files.append((date, time, owner, size, filename))
 				continue
 		print("Could not understand line: " + line)
 		continue
