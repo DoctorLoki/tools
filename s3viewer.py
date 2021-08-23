dcc24bcfa75c48eb6e7bbcc74bf0f60135386bc3
Lachlan Patrick <lachlan.patrick@nearmap.com>
2021-08-23 19:17:50 +1000

    Refactor S3 viewer to use a class.

--- s3viewer.py
+++ s3viewer.py
@@ -21,137 +21,221 @@ GetACLFmtPretty  = "aws s3api get-object-acl --bucket={BUCKET} --key={KEY}"
 ItemFmt = "% 5d) %s %-15s % 12s %-10s %-8s %s"
 
 def main():
-	cache = {}
-	levels = []
-	print(cmdfmt("", "", ListCmdFmtPretty))
-	folders, files = ls("")
-	cache[""] = (folders, files)
-	filtertext = None
-	filterowner = None
-	while True:
-		list_this_folder = True
-		include_owner = False
+	s3viewer = S3Viewer()
+	s3viewer.loop()
+
+class S3Viewer:
+	def __init__(self):
+		self.cache = {}
+		self.levels = []
+		self.folders = []
+		self.files = []
+		self.filter_name = None
+		self.filter_owner = None
+		self.include_owner = False
+
+	def form_path(self):
+		path = "/".join(self.levels)
+		if path != "" and path[-1] != '/':
+			path += "/"
+		return path
+
+	def read_current_path(self):
+		path = self.form_path()
+		print(cmdfmt(path, "", ListCmdFmtPretty))
+		if path in self.cache:
+			self.folders, self.files = self.cache[path]
+		else:
+			self.folders, self.files = ls(path, self.include_owner)
+			self.cache[path] = (self.folders, self.files)
+
+	def print_current_path(self):
+		self.print_folder(self.folders, self.files)
+		self.print_prompt()
+		self.print_filter_settings()
+
+	def print_specific_folder(self, path):
+		if path in self.cache:
+			folders, files = self.cache[path]
+		else:
+			folders, files = ls(path)
+		self.print_folder(folders, files)
+		self.print_prompt()
+		self.print_filter_settings()
+
+	def print_folder(self, folders, files):
 		i = 0
 		for date, time, owner, size, name in folders:
 			i += 1
-			if filtertext and filtertext not in name:
+			if self.filter_name and self.filter_name not in name:
 				continue
-			if filterowner and filterowner not in owner:
+			if self.filter_owner and self.filter_owner not in owner:
 				continue
 			print(ItemFmt % (i, "drwx", owner, "", date, time, name + "/"))
 		for date, time, owner, size, name in files:
 			i += 1
-			if filtertext and filtertext not in name:
+			if self.filter_name and self.filter_name not in name:
 				continue
-			if filterowner and filterowner not in owner:
+			if self.filter_owner and self.filter_owner not in owner:
 				continue
 			print(ItemFmt % (i, "-rw-", owner, size, date, time, name))
-		if len(levels) == 0:
+
+	def print_prompt(self):
+		if len(self.levels) == 0:
 			print("% 5d) exit" % 0)
 		else:
 			print("% 5d) back" % 0)
-		while True:
-			filtertext = None
-			try:
-				sys.stdout.write("Choose: ")
-				sys.stdout.flush()
-				choice = ""
-				while choice == "": 
-					choice = sys.stdin.readline()
-					choice = choice.strip()
-				if choice and choice[:1] == "/":
-					# /text means search for that text in the list of folders and files.
-					filtertext = choice[1:]
-					break
-				if choice and choice[:1] == "@":
-					# @owner means search for that owner in the list of folders and files.
-					filterowner = choice[1:]
-					break
-				if choice == "ls -la":
-					# ls -la means include ownership data in this listing.
-					if path in cache:
-						folder, files = cache[path]
-						if not files or not files[0] or not files[0][2]: # owner is missing
-							del cache[path] # remove cached entry to force loading of owner info
-					include_owner = True
-					break
-				if choice == "ls -laR":
-					# ls -laR means include ownership data and recursively explore all subfolders.
-					list_this_folder = False
-					folders, files = ls_laR("/".join(levels))
-					break
-				if choice == "cd ..":
-					if len(levels) == 0:
-						return
-					levels = levels[:-1]            # cd ..
-					break
-				if choice[:3] == "cd ":
-					subdir = choice[3:]
-					if not subdir:
-						break
-					found = []
-					for date, time, owner, size, foldername in folders:
-						if subdir and subdir[-1] == '*':
-							if foldername[:len(subdir)-1] == subdir[:-1]:
-								found.append(foldername)
-						elif foldername == subdir:
-							found.append(foldername)
-					if len(found) == 1:
-						levels.append(found[0])     # cd subdir
-					else:
-						print("Too many matches, try narrowing your search.")
-						list_this_folder = False
-					break
-				if choice == "pwd":
-					list_this_folder = False
-					print("/".join(levels) + "\n")
-					break
-				num = int(choice)
-				if num == 0:
-					# 0 means go up a level, i.e. cd ..
-					if len(levels) == 0:
-						return
-					levels = levels[:-1]            # cd ..
-					break
-				num -= 1 # ignore 0 option
-				if num < len(folders):
-					# Folders are listed first, so choosing one means go there, i.e. cd foldername
-					levels.append(folders[num][-1]) # cd foldername
-					break
-				num -= len(folders)
-				if num < len(files):
-					# Files are listed next, choosing one asks whether to copy it to local disk.
-					filename = files[num][-1]
-					sys.stdout.write("Copy file: %s ? (y/n) " % (filename))
-					sys.stdout.flush()
-					if sys.stdin.readline().strip().lower()[:1] != 'y':
-						continue
-					# Copy the file to the local file system, within a created directory tree.
-					path = "/".join(levels)
-					destpath = make_all_dirs(path)
-					if destpath == None:
-						continue
-					cmd = cmdfmt(path + "/" + filename, destpath, CopyCmdFmt)
-					print(cmd)
-					os.system(cmd)
-					break
-				print("Invalid option")
-			except:
-				print("\nExiting...")
-				return
 
-		# List folders and files.
-		if not list_this_folder:
-			continue
-		path = "/".join(levels)
-		if path and path[-1] != '/':
-			path += "/"
-		print(cmdfmt(path, "", ListCmdFmtPretty))
-		if path in cache:
-			folders, files = cache[path]
-		else:
-			folders, files = ls(path, include_owner)
-			cache[path] = (folders, files)
+	def print_filter_settings(self):
+		if self.filter_name and self.filter_owner:
+			print(" Note: above filtered by name %r and owner %r" % (self.filter_name, self.filter_owner))
+		elif self.filter_name:
+			print(" Note: above filtered by name %r" % (self.filter_name))
+		elif self.filter_owner:
+			print(" Note: above filtered by owner %r" % (self.filter_owner))
+
+	def loop(self):
+		action = "list"
+		while action != "exit":
+			if action == "list":
+				self.read_current_path()
+				self.print_current_path()
+				# Now clear the name filtering, so it's a once-off.
+				self.filter_name = None
+			action = self.prompt_for_choice()
+
+	def prompt_for_choice(self):
+		self.include_owner = False
+
+		try:
+			sys.stdout.write("Choose: ")
+			sys.stdout.flush()
+			choice = ""
+			while choice == "": 
+				line = sys.stdin.readline()
+				line = line[:-1] if line[-1:] == "\n" else line # Trim off trailing "\n".
+				args = line.split()      # Sometimes we just want an array of args.
+				choice = " ".join(args)  # Sometimes want the line with one space between each arg.
+		except:
+			print("\nExiting...")
+			return "exit"
+
+		if line and line[:1] == "/":
+			# /name means search for that name within the names of folders and files.
+			self.filter_name = line[1:]
+			return "list"
+
+		if line and line[:1] == "@":
+			# @owner means search for that owner within the list of folders and files.
+			self.filter_owner = line[1:]
+			return "list"
+
+		if choice == "ls":
+			return "list"
+
+		if choice in ["ls -la", "ls -a"]:
+			# ls -la means include ownership data in the next listing.
+			path = self.form_path()
+			if path in self.cache:
+				self.folder, self.files = self.cache[path]
+				if not self.files or not self.files[0] or not self.files[0][2]: # owner is missing
+					del self.cache[path] # remove cached entry to force loading of owner info
+			self.include_owner = True
+			return "list"
+
+		if choice in ["ls -laR", "ls -lRa", "ls -alR", "ls -Rla", "ls -aRl", "ls -Ral", "ls -aR", "ls -Ra"]:
+			# ls -laR means include ownership data and recursively explore all subfolders.
+			self.folders, self.files = ls_laR("/".join(self.levels))
+			return "prompt"
+
+		if choice in ["ls *", "ls -l *"]:
+			if len(self.folders) > 10:
+				sys.stdout.write("Are you sure you want to list %d folders? (y/n) " % (len(self.folders)))
+				sys.stdout.flush()
+				if sys.stdin.readline().strip().lower()[:1] != 'y':
+					return "prompt"
+			for folder in self.folders:
+				foldername = folder[-1]
+				path = self.form_path() + foldername
+				if path in self.cache:
+					folders, files = self.cache[path]
+				else:
+					folders, files = ls(path)
+					self.cache[path] = (folders, files)
+				self.print_folder(folders, files)
+			return "prompt"
+
+		if choice == "cd .":
+			return "prompt"
+
+		if choice == "cd ..":
+			if len(self.levels) == 0:
+				return "prompt"
+			self.levels = self.levels[:-1]          # cd ..
+			return "list"
+
+		if len(args) == 2 and args[0] == "cd":
+			subdir = args[1]
+			found = []
+			for date, time, owner, size, foldername in self.folders:
+				if subdir and subdir[-1] == '*':
+					if foldername[:len(subdir)-1] == subdir[:-1]:
+						found.append(foldername)
+				elif foldername == subdir:
+					found.append(foldername)
+			if len(found) == 1:
+				self.levels.append(found[0])        # cd subdir
+				return "list"
+			print("Too many matches, try narrowing your search.")
+			return "prompt"
+
+		if choice == "pwd":
+			print("/".join(self.levels) + "\n")
+			return "prompt"
+
+		try:
+			num = int(choice)
+		except:
+			print("Option not found: %r" % choice)
+			return "prompt"
+
+		if num < 0:
+			print("Invalid option: negative numbers not allowed")
+			return "prompt"
+
+		if num == 0:
+			# 0 means go up a level, i.e. cd ..
+			if len(self.levels) == 0:               # already at root
+				return "prompt"
+			self.levels = self.levels[:-1]          # cd ..
+			return "list"
+
+		num -= 1 # ignore 0 option
+		if num < len(self.folders):
+			# Folders are listed first, so choosing one means go there, i.e. cd foldername
+			self.levels.append(self.folders[num][-1]) # cd foldername
+			return "list"
+
+		num -= len(self.folders)
+		if num < len(self.files):
+			# Files are listed next, choosing one asks whether to copy it to local disk.
+			filename = self.files[num][-1]
+			sys.stdout.write("Copy file: %s ? (y/n) " % (filename))
+			sys.stdout.flush()
+			if sys.stdin.readline().strip().lower()[:1] != 'y':
+				return "prompt"
+			# Copy the file to the local file system, within a created directory tree.
+			path = self.form_path()
+			destpath = make_all_dirs(path)
+			if destpath == None:
+				return "prompt"
+			cmd = cmdfmt(path + "/" + filename, destpath, CopyCmdFmt)
+			print(cmd)
+			os.system(cmd)
+			return "prompt"
+
+		print("Invalid option")
+		return "prompt"
 
 def get_owner(bucket, key):
 	cmd = GetACLFmt.replace("{BUCKET}", bucket).replace("{KEY}", key)
